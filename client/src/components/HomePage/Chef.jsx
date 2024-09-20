b9cf91c34854108aa1f50fc7b6807dca509b8494
vince <vincedominic.delapena@gmail.com>
2024-09-21 00:23:05 +0800

    complete chefs section responsiveness

--- client/src/components/HomePage/Chef.jsx
+++ client/src/components/HomePage/Chef.jsx
@@ -5,39 +5,33 @@ import chef_3 from "../../assets/chef_3.jpg";
 function Chef() {
   return (
     <div className="w-full flex items-center justify-center flex-col p-20 bg-[#F9F9F9]">
-      <h2 className="text-5xl font-bold">Our Popular Chef</h2>
-      <div className="flex items-center justify-center gap-4 mt-6">
+      <h2 className="text-5xl font-bold mb-10">Our Popular Chef</h2>
+      <div className="flex items-center justify-center gap-10 mt-6 flex-wrap">
         <div className="flex flex-col items-center justify-center text-lg">
-          <img
-            className="w-[300px] h-[500px] object-contain"
-            src={chef_1}
-            alt="chef"
-          />
+          <div className="w-[300px] h-[400px] overflow-hidden rounded-lg mb-4">
+            <img className="object-cover" src={chef_1} alt="chef" />
+          </div>
           <p className="font-bold">Betran Komar</p>
           <p className="text-slate-500 font-semibold">Head chef</p>
         </div>
 
         <div className="flex flex-col items-center justify-center text-lg">
-          <img
-            className="w-[300px] h-[500px] object-contain"
-            src={chef_3}
-            alt="chef"
-          />
+          <div className="w-[300px] h-[400px] overflow-hidden rounded-lg mb-4">
+            <img className="object-cover" src={chef_3} alt="chef" />
+          </div>
           <p className="font-bold">Iswan Dracho</p>
           <p className="text-slate-500 font-semibold">Chef</p>
         </div>
 
         <div className="flex flex-col items-center justify-center text-lg">
-          <img
-            className="w-[300px] h-[500px] object-contain"
-            src={chef_2}
-            alt="chef"
-          />
+          <div className="w-[300px] h-[400px] overflow-hidden rounded-lg mb-4">
+            <img className="object-cover" src={chef_2} alt="chef" />
+          </div>
           <p className="font-bold">Ferry Sauwi</p>
           <p className="text-slate-500 font-semibold">Chef</p>
         </div>
       </div>
-      <button className="px-16 py-4 bg-red-500 text-lg font-bold text-white mt-12 rounded-lg">
+      <button className="px-16 py-4 bg-red-500 text-lg font-bold text-white mt-12 rounded-lg w-full md:w-[300px]">
         View all
       </button>
     </div>
