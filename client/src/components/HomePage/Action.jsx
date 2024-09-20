0ad273f2013f0a1c92e77fd5e2e82711f01048a5
vince <vincedominic.delapena@gmail.com>
2024-09-21 00:31:19 +0800

    complete action section responsiveness

--- client/src/components/HomePage/Action.jsx
+++ client/src/components/HomePage/Action.jsx
@@ -1,15 +1,15 @@
 function Action() {
   return (
-    <div className="bg-[#F9F9F9] flex items-center justify-center p-20">
-      <div className="flex items-center justify-center flex-col gap-10 border py-16 px-44 rounded-2xl bg-[#FDD9D9]">
-        <h2 className="font-bold text-4xl text-[#311F09]">
+    <div className="bg-[#F9F9F9] flex items-center justify-center p-20 ">
+      <div className="flex items-center justify-center flex-col gap-10 border p-10 lg:px-40 lg:py-20 rounded-2xl bg-[#FDD9D9]">
+        <h2 className="font-bold text-4xl text-[#311F09] text-center md:text-start">
           Hungry? We are open now..
         </h2>
-        <div className="flex items-center justify-center gap-6">
-          <button className="text-white px-10 font-semibold text-lg bg-red-500 py-3 rounded-xl">
+        <div className="flex items-center justify-center gap-2 sm:gap-10 flex-wrap">
+          <button className="text-white px-4 sm:px-10 font-semibold  text-xs sm:text-lg bg-red-500 py-3 rounded-xl">
             Order now
           </button>
-          <button className="text-red-500 px-10 font-semibold text-lg bg-[#FEECEC] py-3 rounded-xl">
+          <button className="text-red-500 px-4 sm:px-10 font-semibold text-xs sm:text-lg bg-[#FEECEC] py-3 rounded-xl">
             Reservation
           </button>
         </div>
