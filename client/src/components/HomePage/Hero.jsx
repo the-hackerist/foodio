37e16b4d1f7573c73f47cc357a3bbd7359a8115c
vince <vincedominic.delapena@gmail.com>
2024-09-20 23:51:28 +0800

    complete hero section responsiveness

--- client/src/components/HomePage/Hero.jsx
+++ client/src/components/HomePage/Hero.jsx
@@ -2,28 +2,28 @@ import img from "../../assets/pasta.png";
 
 function Hero() {
   return (
-    <div className="bg-gradient-to-b from-[#F8DBDC] via-[#F8E7E7]to-[#F9F9F9] h-screen py-20 flex items-center justify-center gap-2">
+    <div className="bg-gradient-to-b from-[#F8DBDC] via-[#F8E7E7]to-[#F9F9F9] h-screen py-20 flex items-center justify-center gap-2 lg:gap-10 md:px-20">
       <div className="flex justify-center flex-col">
-        <h1 className="text-5xl font-bold leading-snug">
+        <h1 className="text-5xl font-bold leading-snug lg:text-6xl">
           Best Restaurant
           <br />
           In <span className="text-red-500">Town.</span>
         </h1>
-        <p className="w-[440px] text-[#5C4429] mt-6 text-lg">
+        <p className="w-[440px] md:w-[340px] lg:w-[450px] text-[#5C4429] mt-6 text-lg md:text-base lg:text-lg">
           We provide best food in town, we provide home delivery and dine in
           services.
         </p>
-        <div className="flex gap-4 mt-10">
-          <button className="text-white px-10 font-semibold text-lg bg-red-500 py-3 rounded-xl">
+        <div className="flex gap-4 mt-10 md:gap-2">
+          <button className="text-white px-10 font-semibold text-lg bg-red-500 py-3 rounded-xl md:px-4 md:text-base lg:px-10 lg:text-lg">
             Order now
           </button>
-          <button className="text-red-500 px-10 font-semibold text-lg bg-[#F9E1E1] py-3 rounded-xl">
+          <button className="text-red-500 px-10 font-semibold text-lg bg-[#F9E1E1] py-3 rounded-xl md:px-4 md:text-base lg:px-10 lg:text-lg">
             Reservation
           </button>
         </div>
       </div>
 
-      <div className="flex items-center justify-center">
+      <div className="hidden md:flex items-center justify-center">
         <img className="w-[350px] object-contain" src={img} alt="hero img" />
       </div>
     </div>
