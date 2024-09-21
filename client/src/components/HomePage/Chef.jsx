2de297ae9e1ef50fa59a09d23d7a880f7ce2e015
vince <vincedominic.delapena@gmail.com>
2024-09-22 00:39:45 +0800

    fix text responsiveness in chef section of homepage and complete about us page responsiveness

--- client/src/components/HomePage/Chef.jsx
+++ client/src/components/HomePage/Chef.jsx
@@ -5,7 +5,9 @@ import chef_3 from "../../assets/chef_3.jpg";
 function Chef() {
   return (
     <div className="w-full flex items-center justify-center flex-col p-20 bg-[#F9F9F9]">
-      <h2 className="text-5xl font-bold mb-10">Our Popular Chef</h2>
+      <h2 className="text-5xl font-bold mb-10 text-center sm:text-start">
+        Our Popular Chef
+      </h2>
       <div className="flex items-center justify-center gap-10 mt-6 flex-wrap">
         <div className="flex flex-col items-center justify-center text-lg">
           <div className="w-[300px] h-[400px] overflow-hidden rounded-lg mb-4">
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -4,22 +4,22 @@ import owner from "../assets/owner.jpg";
 
 function AboutUs() {
   return (
-    <div className="bg-[#F9F9F9] px-20 pt-40 pb-20">
-      <div className="flex items-center gap-32 justify-center">
-        <div className="w-[475px] h-[475px] bg-[#F5F5F5] flex items-center justify-center rounded-full">
-          <div className="w-[400px] h-[400px] bg-[#EEEDEC] flex items-center justify-center rounded-full ">
-            <div className="w-[325px] h-[325px] flex items-center rounded-full overflow-hidden">
+    <div className="bg-[#F9F9F9] px-20 md:px-4 lg:px-20 pt-20 pb-20 flex flex-col gap-10">
+      <div className="flex items-center gap-4 lg:gap-20 justify-center flex-col md:flex-row">
+        <div className="lg:w-[475px] lg:h-[475px] w-[350px] h-[350px] bg-[#F5F5F5] flex items-center justify-center rounded-full">
+          <div className="lg:w-[400px] lg:h-[400px] w-[300px] h-[300px] bg-[#EEEDEC] flex items-center justify-center rounded-full ">
+            <div className="lg:w-[325px] lg:h-[325px] w-[250px] h-[250px] flex items-center rounded-full overflow-hidden">
               <img className="" src={img} alt="restaurant image" />
             </div>
           </div>
         </div>
 
-        <div>
+        <div className="flex items-center flex-col md:items-start">
           <h2 className="text-5xl font-bold leading-snug">
             About Our <br />
             <span className="text-red-500">Restaurant</span>
           </h2>
-          <p className="text-lg w-[340px] mt-4 text-[#5C4429]">
+          <p className="text-xl md:text-lg w-[350px] mt-4 text-[#5C4429] text-center md:text-start">
             This dish is full of flavor and nutrition! Quinoa is a complete
             protein, providing all the essential amino acids your body needs,
             and is also a good source of fiber.
@@ -30,8 +30,8 @@ function AboutUs() {
         </div>
       </div>
 
-      <div className="flex items-center gap-32 justify-center mt-40">
-        <p className="text-lg w-[350px] mt-4 text-[#5C4429]">
+      <div className="flex items-center gap-4 lg:gap-20 justify-center flex-col md:flex-row">
+        <p className="w-[350px] mt-4 text-[#5C4429] text-center md:text-start text-xl md:text-lg">
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
@@ -39,9 +39,9 @@ function AboutUs() {
           aspernatur aut odit aut fugit.
         </p>
 
-        <div className="w-[475px] h-[475px] bg-[#F5F5F5] flex items-center justify-center rounded-full">
-          <div className="w-[400px] h-[400px] bg-[#EEEDEC] flex items-center justify-center rounded-full ">
-            <div className="w-[325px] h-[325px] flex items-center rounded-full overflow-hidden">
+        <div className="lg:w-[475px] lg:h-[475px] w-[350px] h-[350px] bg-[#F5F5F5] flex items-center justify-center rounded-full">
+          <div className="lg:w-[400px] lg:h-[400px] w-[300px] h-[300px] bg-[#EEEDEC] flex items-center justify-center rounded-full ">
+            <div className="lg:w-[325px] lg:h-[325px] w-[250px] h-[250px] flex items-center rounded-full overflow-hidden">
               <img
                 className="bg-red-400 scale-150"
                 src={food}
@@ -52,18 +52,19 @@ function AboutUs() {
         </div>
       </div>
 
-      <div className="flex items-center justify-center gap-16 mt-40">
+      <div className="flex items-center justify-center gap-8 lg:gap-20 flex-col md:flex-row">
         <img
-          className="w-[375px] h-[425px] object-contain"
+          className="w-[375px] h-[425px] object-cover rounded-xl shadow-md"
           src={owner}
           alt="owner image"
         />
-        <div className="w-[500px]">
-          <h2 className="text-5xl font-bold">
+
+        <div className="flex flex-col items-center text-center md:text-start md:w-[350px]">
+          <h2 className="text-4xl md:text-5xl font-bold ">
             <span className="text-red-500">Owner </span>& Executive Chef
           </h2>
-          <p className="mt-4 text-3xl">Ismail Marzuki</p>
-          <p className="text-2xl font-thin italic mt-8 w-[320px] leading-10">
+          <p className="mt-4 text-2xl md:text-2xl w-full">Ismail Marzuki</p>
+          <p className="text-2xl font-thin italic mt-8 leading-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
