e5ae453a0d733552573981e69e87dfe404e93c6d
vince <vincedominic.delapena@gmail.com>
2024-09-22 19:33:16 +0800

    complete reservation page responsiveness

--- client/src/pages/Reservation.jsx
+++ client/src/pages/Reservation.jsx
@@ -2,32 +2,35 @@ import food from "../assets/food-2.jpg";
 
 function Reservation() {
   return (
-    <div className="py-20">
-      <div className="h-screen flex items-center justify-center gap-10">
-        <div className="w-[450px] h-[550px] overflow-hidden rounded-3xl">
-          <img className="object-cover" src={food} alt="restaurant image" />
+    <div className="pt-20">
+      <div className="p-10 md:pt-20  flex flex-row items-center justify-center gap-10">
+        <div className="w-[450px] h-[550px] overflow-hidden rounded-3xl hidden lg:inline-block">
+          <img className="object-contain" src={food} alt="restaurant image" />
         </div>
 
-        <div className="flex flex-col items-center justify-between gap-6 h-[550px] w-[350px]">
+        <div className="flex flex-col items-center md:justify-between gap-10 md:h-[550px] md:w-[350px] text-center md:text-start">
           <h2 className="text-5xl font-bold">Book a table</h2>
+
           {/* date selector */}
-          {/* time selector */}
           <input
-            className="p-6 w-full border rounded-xl"
+            className="py-4 px-6 md:p-6 w-full border rounded-xl"
             type="text"
             placeholder="date"
           />
+
+          {/* time selector */}
           <input
-            className="p-6 w-full border rounded-xl"
+            className="py-4 px-6 md:p-6 w-full border rounded-xl"
             type="text"
             placeholder="time"
           />
           <input
-            className="p-6 w-full border rounded-xl"
+            className="py-4 px-6 md:p-6 w-full border rounded-xl"
             type="number"
-            placeholder="part size"
+            defaultValue={1}
+            placeholder="party size"
           />
-          <button className="p-6 text-white font-bold bg-red-500 w-full rounded-xl">
+          <button className="py-4 px-6 md:p-6 text-white font-bold bg-red-500 w-full rounded-xl">
             Book now
           </button>
         </div>
